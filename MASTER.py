816ac2b436f1b4572c2a482d8af6b07d9eda4918
jonschnell <31516901+jonschnell@users.noreply.github.com>
2019-10-21 17:07:16 -0500

    added MASTER.py

---
+++ MASTER.py
@@ -0,0 +1,22 @@
+print('hello')
+a="string"
+print(a)
+del(a)
+a=float(input("enter a number\n"))
+print(a)
+booleana= 6 == a
+print(booleana)
+if booleana == True:
+    print('truth is always the answer')
+else:
+    print('you liar')
+i=1
+while i<=5:
+    print(i)
+    i+=1
+
+g=0
+lista=["item 1", "item 2"]
+while g<=1:
+    print(lista[g])
+    g+=1
