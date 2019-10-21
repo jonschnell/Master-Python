8df3bc1e5fa09a15a5a2150e27a3ee26deb4ac9c
jonschnell <31516901+jonschnell@users.noreply.github.com>
2019-10-21 18:45:58 -0500

    more comments and beutification

--- MASTER.py
+++ MASTER.py
@@ -4,28 +4,35 @@
 print('hello')
 
 #string variable
-a="string"
+a = "string"
+#print variable
 print(a)
 #removes variable a
 del(a)
 
 #user I/O
-a=float(input("enter a number\n"))
+a = float(input("enter a number\n"))
 print(a)
 
-booleana= 6 == a
+#define boolean
+booleana = (6 == a)
 print(booleana)
+#if else statment
 if booleana == True:
     print('truth is always the answer')
 else:
     print('you liar')
-i=1
-while i<=5:
+    
+i = 1
+#while statment
+while i <= 5:
     print(i)
-    i+=1
+    i += 1
 
-g=0
-lista=["item 1", "item 2"]
-while g<=1:
+
+g = 0
+#list/array delectration
+lista = ["item 1", "item 2"]
+while g <= 1:
     print(lista[g])
-    g+=1
+    g += 1
